552ece0131ef9f0507378c1c16ecf62e389b3c67
UxDeveloper82 <electronicsmf@aol.com>
2021-03-05 20:23:03 -0500

    app.js search function

--- app.js
+++ app.js
@@ -1,12 +1,23 @@
-
 const charactersList = document.getElementById('charactersList');
+const searchBar = document.getElementById('searchBar');
 let hpCharacters = [];
+
+console.log(searchBar);
+searchBar.addEventListener('keyup', (e) => {
+    const searchString = e.target.value.toLowerCase();
+    const filteredCharacters = hpCharacters.filter( character => {
+       return character.name.toLowerCase().includes(searchString) ||  
+              character.house.toLowerCase().includes(searchString);
+    });
+    displayCharacters(filteredCharacters);
+})
 
 const loadCharacters = async () => {
     try {
         const res = await fetch('https://hp-api.herokuapp.com/api/characters');
         hpCharacters = await res.json();
         displayCharacters(hpCharacters);
+        console.log(hpCharacters);
     } catch (err) {
         console.error(err);
     }
